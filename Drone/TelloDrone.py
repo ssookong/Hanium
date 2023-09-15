0e6268bedda2873017371411f5e05ca7f4762f91
ssookong <129821775+ssookong@users.noreply.github.com>
2023-09-15 19:53:07 +0900

    Add files via upload

--- Drone/TelloDrone.py
+++ Drone/TelloDrone.py
@@ -1,10 +1,12 @@
 import cv2
+import time
 from djitellopy import Tello
 tello = Tello()
 tello.connect()
 tello.streamon()
 tello.takeoff()  # 이륙
 tello.move_forward(50)  # 앞으로 50cm 이동
+time.sleep(3)
 cap=cv2.VideoCapture('udp://@0.0.0:11111')
 ret, frame=cap.read()
 if ret:
@@ -12,6 +14,7 @@ if ret:
 tello.rotate_clockwise(90)  # 오른쪽으로 90도 회전
 tello.move_forward(20)  # 앞으로 20cm 이동
 tello.rotate_counter_clockwise(90)
+time.sleep(3)
 cap2=cv2.VideoCapture('udp://@0.0.0:11111')
 ret2, frame2=cap2.read()
 if ret2:
@@ -19,6 +22,7 @@ if ret2:
 tello.rotate_clockwise(90)  # 오른쪽으로 90도 회전
 tello.move_forward(20)  # 앞으로 20cm 이동
 tello.rotate_counter_clockwise(90)
+time.sleep(3)
 cap3=cv2.VideoCapture('udp://@0.0.0:11111')
 ret3, frame3=cap3.read()
 if ret3:
